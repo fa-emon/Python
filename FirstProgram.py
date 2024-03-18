44472a882a640ccee918431d7683a27a777f854c
Farhan933 <farhanfarabiemon@gmail.com>
2024-03-18 22:04:18 +0600

    taking input

---
+++ FirstProgram.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+name= 'Farhan'
+age= 23
+studentID = 1902001
+isValid = True
+
+print(type(name), type(age), type(studentID))
---
+++ input.py
@@ -0,0 +1,6 @@
+#taking input from user and printing it.
+name = input("name: ")
+age = int(input("age: "))
+price = float(input("price: "))
+
+print("My name is", name, "and I'm", age, "years old")
